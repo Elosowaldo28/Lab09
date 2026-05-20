9baf2cd0cc882bebf9ab23e6d17cf893651e256d
Elosowaldo28 <osowal28@gmail.com>
2026-05-19 23:07:03 -0500

    Paso 5.1.2: MainActivity actualizado con ProgPrincipal9 y Retrofit configurado

--- app/src/main/java/com/example/lab09/MainActivity.kt
+++ app/src/main/java/com/example/lab09/MainActivity.kt
@@ -5,18 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.example.lab09.ui.theme.Lab09Theme
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val retrofit = Retrofit.Builder()
-    .baseUrl("https://jsonplaceholder.typicode.com/")
-    .addConverterFactory(GsonConverterFactory.create())
-    .build()
-
-val postApiService = retrofit.create(PostApiService::class.java)
+val urlBase = "https://json-placeholder.mock.beeceptor.com/"
+val retrofit = Retrofit.Builder().baseUrl(urlBase)
+    .addConverterFactory(GsonConverterFactory.create()).build()
+val servicio = retrofit.create(PostApiService::class.java)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,8 +28,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    ProgPrincipal9()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ProgPrincipal9() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar =    { BarraSuperior() },
+        bottomBar = { BarraInferior(navController) },
+        content =   { paddingValues -> Contenido(paddingValues, navController, servicio) }
+    )
 }
