0145e04fbf46b9a10ce568e71b2c45cd48314fc4
Elosowaldo28 <osowal28@gmail.com>
2026-05-19 23:10:59 -0500

    Paso 5.3: Agregadas BarraSuperior y BarraInferior con Scaffold

--- app/src/main/java/com/example/lab09/MainActivity.kt
+++ app/src/main/java/com/example/lab09/MainActivity.kt
@@ -13,6 +13,19 @@ import androidx.navigation.compose.rememberNavController
 import com.example.lab09.ui.theme.Lab09Theme
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.navigation.NavHostController
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 
 val urlBase = "https://json-placeholder.mock.beeceptor.com/"
 val retrofit = Retrofit.Builder().baseUrl(urlBase)
@@ -48,4 +61,40 @@ fun ProgPrincipal9() {
         bottomBar = { BarraInferior(navController) },
         content =   { paddingValues -> Contenido(paddingValues, navController, servicio) }
     )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BarraSuperior() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "JSONPlaceHolder Access",
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+@Composable
+fun BarraInferior(navController: NavHostController) {
+    NavigationBar(
+        containerColor = Color.LightGray
+    ) {
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Home, contentDescription = "Inicio") },
+            label = { Text("Inicio") },
+            selected = navController.currentDestination?.route == "inicio",
+            onClick = { navController.navigate("inicio") }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Favorite, contentDescription = "Posts") },
+            label = { Text("Posts") },
+            selected = navController.currentDestination?.route == "posts",
+            onClick = { navController.navigate("posts") }
+        )
+    }
 }
