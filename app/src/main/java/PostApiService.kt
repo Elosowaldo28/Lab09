44380a4b704e7529994718da71ba5d3d9f3b689d
Elosowaldo28 <osowal28@gmail.com>
2026-05-19 23:28:38 -0500

    Paso 9

--- app/src/main/java/PostApiService.kt
+++ app/src/main/java/PostApiService.kt
@@ -1,8 +1,14 @@
 package com.example.lab09
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PostApiService {
+
     @GET("posts")
-    suspend fun getPosts(): List<PostModel>
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id}")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
 }
